48392e7c951099a1398ebd7710af9c1de3968824
gholley15 <gholley@crimson.ua.edu>
2016-10-04 16:13:38 -0500

    Gabby: Updated bars

--- app/src/main/java/com/gimbal/hello_gimbal_android/About.java
+++ app/src/main/java/com/gimbal/hello_gimbal_android/About.java
@@ -1,7 +1,14 @@
 package com.gimbal.hello_gimbal_android;
 
+import android.content.Intent;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
 
 public class About extends AppCompatActivity {
 
@@ -9,6 +16,5 @@ public class About extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
-
     }
 }
